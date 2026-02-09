6dca00f36f6b99184201300f43f553821aada436
Aelaf Eskindir Abebe <aelaf@aelaf.aelaf.com>
2026-02-09 23:54:16 +0300

    Express added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello There!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+
+export default app;
